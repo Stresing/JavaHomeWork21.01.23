5727ecc117f9c6ec78b57eaaeb96996aba0ba707
stresing <Stresingrad@gmail.com>
2023-01-21 22:24:44 +0700

    final task1 and task2

--- program.java
+++ program.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class program {
     public static void main(String[] args) {
-        task1();
+        task2();
 
     }
 
     static void task1() {
         System.out.println("Введите число: ");
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);5
         int n = scanner.nextInt();
         int sum = 0;
         for (int i = 1; i <= n; i++) {
@@ -16,5 +16,16 @@ public class program {
 
         }
         System.out.println(sum);
+    }
+
+    static void task2() {
+        System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int mult = 1; // multiplication
+        for (int i = 1; i <= n; i++) {
+            mult = mult * i;
+        }
+        System.out.println(mult);
     }
 }
